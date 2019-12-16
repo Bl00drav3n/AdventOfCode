af1d967a58ec557d6f2546da5596f1ce4ff978ae
Rav3n <bl00drav3nz@gmail.com>
2019-12-16 20:34:30 +0100

    Add better bad FFT implementation.

--- 2019/16/code.go
+++ 2019/16/code.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"time"
 )
 
 func toDigits(s string) (digits []int) {
@@ -13,7 +14,7 @@ func toDigits(s string) (digits []int) {
 	return
 }
 
-func fft(digitsIn []int) (digitsOut []int) {
+func worstFFT(digitsIn []int) (digitsOut []int) {
 	digitsOut = make([]int, len(digitsIn))
 	pattern := []int{0, 1, 0, -1}
 	for i := range digitsOut {
@@ -31,12 +32,57 @@ func fft(digitsIn []int) (digitsOut []int) {
 	return
 }
 
-func part1(input string) {
-	digits := toDigits(input)
-	for i := 0; i < 100; i++ {
-		digits = fft(digits)
+func min(a, b int) int {
+	if a < b {
+		return a
 	}
-	fmt.Printf("After %3d phases of FFT: %v\n", 100, digits[:8])
+	return b
+}
+
+func sum(digits []int, pos, block int) (result int) {
+	if pos < len(digits) {
+		for i := pos; i < len(digits); i += 4 * block {
+			if i+block > len(digits) {
+				block = len(digits) - i
+			}
+			for j := 0; j < block; j++ {
+				result += digits[i+j]
+			}
+		}
+	}
+	return
+}
+
+func betterFFT(digitsIn []int) (digitsOut []int) {
+	// NOTE: Still O(n²)
+	digitsOut = make([]int, len(digitsIn))
+	for i := range digitsOut {
+		var value int
+		value = sum(digitsIn, i, i+1) - sum(digitsIn, i+2*(i+1), i+1)
+		value %= 10
+		if value < 0 {
+			digitsOut[i] = -value
+		} else {
+			digitsOut[i] = value
+		}
+	}
+	return
+}
+
+func part1(input string, phases int) {
+	digits := toDigits(input)
+	start := time.Now()
+	for i := 0; i < phases; i++ {
+		digits = betterFFT(digits)
+	}
+	fmt.Printf("After %3d phases of FFT (len=%d): %v\n", phases, len(digits), digits[:8])
+	fmt.Println(time.Since(start))
+}
+
+func part2(input string, phases int) {
+	// TODO
+	// NOTE: Some rows reduce to zero if aligned properly
+	// NOTE: Some rows take a simple form of repeats * FFT(original) + rest
 }
 
 func main() {
@@ -44,5 +90,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	part1(string(signal))
+	part1(string(signal), 100)
+	part2(string(signal), 100)
 }
