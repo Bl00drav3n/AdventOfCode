18849dbb3a4807e927746ea865a383fe1357ee80
Rav3n <bl00drav3nz@gmail.com>
2019-12-16 19:25:41 +0100

    Fix format.

--- 2019/16/code.go
+++ 2019/16/code.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
+	"fmt"
+	"io/ioutil"
 )
 
 func toDigits(s string) (digits []int) {
-  digits = make([]int, len(s))
-  for i := range s {
-    digits[i] = int(s[i] - '0')
-  }
-  return
+	digits = make([]int, len(s))
+	for i := range s {
+		digits[i] = int(s[i] - '0')
+	}
+	return
 }
 
 func fft(digitsIn []int) (digitsOut []int) {
-  digitsOut = make([]int, len(digitsIn))
-  pattern := []int{0, 1, 0, -1}
-  for i := range digitsOut {
-    var value int
-    for j := i; j < len(digitsOut); j++ {
-      value += digitsIn[j] * pattern[((j + 1) / (i + 1)) % len(pattern)]
-    }
-    value %= 10
-    if value < 0 {
-      digitsOut[i] = -value
-    } else {
-      digitsOut[i] = value
-    }
-  }
-  return
+	digitsOut = make([]int, len(digitsIn))
+	pattern := []int{0, 1, 0, -1}
+	for i := range digitsOut {
+		var value int
+		for j := i; j < len(digitsOut); j++ {
+			value += digitsIn[j] * pattern[((j+1)/(i+1))%len(pattern)]
+		}
+		value %= 10
+		if value < 0 {
+			digitsOut[i] = -value
+		} else {
+			digitsOut[i] = value
+		}
+	}
+	return
 }
 
 func part1(input string) {
-  digits := toDigits(input)
-  for i := 0; i < 100; i++ {
-    digits = fft(digits)
-  }
-  fmt.Printf("After %3d phases of FFT: %v\n", 100, digits[:8])
+	digits := toDigits(input)
+	for i := 0; i < 100; i++ {
+		digits = fft(digits)
+	}
+	fmt.Printf("After %3d phases of FFT: %v\n", 100, digits[:8])
 }
 
 func main() {
-  signal, err := ioutil.ReadFile("input.txt")
-  if err != nil {
-	  panic(err)
-  }
-  part1(string(signal))
+	signal, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	part1(string(signal))
 }
