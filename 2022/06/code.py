a2e515567d3e1a5efb1f14214b3bc144fb984fa6
Bl00drav3n <bl00drav3nz@gmail.com>
2022-12-06 12:41:43 +0100

    Update day 6

--- 2022/06/code.py
+++ 2022/06/code.py
@@ -1,23 +1,20 @@
 test_input = '''mjqjpqmgbljsphdztnvjfqwrcgsmlb'''
 
-def test_start(window):
-    return len(set(window)) == 4
+START_WINDOW_SIZE = 4
+MSG_WINDOW_SIZE = 14
 
-def test_msg(window):
-    return len(set(window)) == 14
-
-def test(input, test_fn, window_size):
+def test(input, window_size):
     for i in range(0, len(input) - window_size):
         window = input[i:i+window_size]
-        if test_fn(window):
+        if len(set(window)) == window_size:
             return i + window_size
 
 def part1(input):
-    first = test(input, test_start, 4)
+    first = test(input, START_WINDOW_SIZE)
     print("Part 1: {}".format(first))
 
 def part2(input):
-    first = test(input, test_msg, 14)
+    first = test(input, MSG_WINDOW_SIZE)
     print("Part 2: {}".format(first))
 
 print('---TEST---')
