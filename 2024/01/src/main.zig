5372252ef495e3223d13ae4d2eeb308fb4344872
Bl00drav3n <bl00drav3nz@gmail.com>
2024-12-02 02:04:11 +0100

    Update day 1

--- 2024/01/src/main.zig
+++ 2024/01/src/main.zig
@@ -11,19 +11,13 @@ const test_input =
 ;
 
 const Pair = struct { a: i32, b: i32 };
+const Lists = struct { left: []i32, right: []i32, count: usize };
 
-fn get_next(comptime T: type, it: *T) []const u8 {
-    if (it.*.next()) |string| {
-        return string;
-    }
-    return "";
-}
-
-fn ordered_pair_from_line(line: []const u8) !Pair {
+fn pair_from_line(line: []const u8) !Pair {
     var it = std.mem.splitSequence(u8, line, "   ");
     return Pair{
-        .a = try std.fmt.parseInt(i32, get_next(@TypeOf(it), &it), 10),
-        .b = try std.fmt.parseInt(i32, get_next(@TypeOf(it), &it), 10),
+        .a = try std.fmt.parseInt(i32, it.next() orelse "", 10),
+        .b = try std.fmt.parseInt(i32, it.next() orelse "", 10),
     };
 }
 
@@ -37,30 +31,16 @@ fn count_occurences(items: []const i32, value: i32) i32 {
     return sum;
 }
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+fn init(allocator: std.mem.Allocator, input_data: []const u8) !Lists {
     var pairs = std.ArrayList(Pair).init(allocator);
-
-    // For testing uncomment this and comment out input data
-    //var reader = std.io.fixedBufferStream(test_input);
-    //var stream = reader.reader();
-
-    // The input data
-    var file = try std.fs.cwd().openFile("input.txt", .{});
-    var reader = std.io.bufferedReader(file.reader());
-    var stream = reader.reader();
-
-    var line = std.ArrayList(u8).init(allocator);
-    const line_writer = line.writer();
-    while (stream.streamUntilDelimiter(line_writer, '\n', null)) {
-        defer line.clearRetainingCapacity();
-        try pairs.append(try ordered_pair_from_line(line.items));
-    } else |err| switch (err) {
-        error.EndOfStream => {},
-        else => return err,
+    var it = std.mem.splitScalar(u8, input_data, '\n');
+    while (it.next()) |line| {
+        if (line.len > 0) {
+            const pair = try pair_from_line(line);
+            try pairs.append(pair);
+        }
     }
-
+    pairs = pairs;
     var lvalues: []i32 = try allocator.alloc(i32, pairs.items.len);
     var rvalues: []i32 = try allocator.alloc(i32, pairs.items.len);
     for (pairs.items, 0..) |pair, i| {
@@ -71,16 +51,43 @@ pub fn main() !void {
     std.mem.sort(i32, lvalues, {}, std.sort.asc(i32));
     std.mem.sort(i32, rvalues, {}, std.sort.asc(i32));
 
+    return .{ .left = lvalues, .right = rvalues, .count = pairs.items.len };
+}
+
+fn part1(allocator: std.mem.Allocator, input_data: []const u8) !void {
+    const lists = try init(allocator, input_data);
     var sum: u32 = 0;
-    for (0..pairs.items.len) |i| {
-        sum += @abs(lvalues[i] - rvalues[i]);
+    for (0..lists.count) |i| {
+        sum += @abs(lists.left[i] - lists.right[i]);
     }
     std.debug.print("Part1: The total distance between the lists is {d}.\n", .{sum});
+}
 
+fn part2(allocator: std.mem.Allocator, input_data: []const u8) !void {
+    const lists = try init(allocator, input_data);
     var score: i32 = 0;
-    for (lvalues) |lvalue| {
-        const count = count_occurences(rvalues, lvalue);
+    for (lists.left) |lvalue| {
+        const count = count_occurences(lists.right, lvalue);
         score += count * lvalue;
     }
     std.debug.print("Part2: The similarity score of the lists is {d}.\n", .{score});
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const buffer: []u8 = try allocator.alloc(u8, 1024 * 1024 * 1024);
+
+    // The input data
+    var file = try std.fs.cwd().openFile("input.txt", .{});
+    const size = try file.readAll(buffer);
+    const input_data = buffer[0..size];
+
+    std.debug.print("---TEST DATA---\n", .{});
+    try part1(allocator, test_input);
+    try part2(allocator, test_input);
+
+    std.debug.print("---INPUT DATA---\n", .{});
+    try part1(allocator, input_data);
+    try part2(allocator, input_data);
 }
